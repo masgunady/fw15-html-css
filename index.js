9dae9892f796b9a25b1f117087bf97d53c2174cd
Gunadi PS <gndsulistiawan@gmail.com>
2023-03-16 00:31:06 +0700

    menyelesaikan dashboar page dan mulai implementasi tailwind untuk responsive

--- index.js
+++ index.js
@@ -42,3 +42,44 @@ function ubahHurufKonsonan(kata) {
 }
 
 console.log(ubahHurufKonsonan('12344'));
+
+let dataDiri = {
+    name: "Gunadi Pratama Sulistiawan",
+    age: 26,
+    hoobues: ["Olraga", "Music"],
+    is_married: true,
+    school_list: [{
+            major: "SMK",
+            year_in: "2011",
+            year_out: "2014"
+        },
+        {
+            major: "Bachelor Degree",
+            year_in: "2014",
+            year_out: "2019"
+        }
+    ],
+    skill: [{
+            skill_name: "Laravel",
+            level: "Junior"
+        },
+        {
+            skill_name: "CI",
+            level: "Junior"
+        }
+    ],
+    interesting_coding: true
+
+}
+
+console.log(dataDiri);
+
+
+const mtk = 80;
+const bahasaIndonesia = 90;
+const bahasaInggris = 89;
+const ipa = 69;
+
+let nilaiUN = [mtk, bahasaIndonesia, bahasaInggris, ipa];
+
+rataRata = Sum
